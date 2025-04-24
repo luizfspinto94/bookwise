8cb47e6f0bc9d21ffd445e496c8d43ed3e230161
luizfspinto94 <luizfspinto94@gmail.com>
2025-04-24 20:42:37 -0300

    criando a pagina de livro individual

---
+++ dados.php
@@ -0,0 +1,78 @@
+<?php
+
+$livros = [
+    [
+        "id" => 1,
+        "titulo" => "O Mistério da Biblioteca Fantasma",
+        "imagem" => "https://exemplo.com/imagens/biblioteca-fantasma.jpg",
+        "autor" => "Carla Mendes",
+        "avaliacao" => 5,
+        "descricao" => "Uma jovem bibliotecária descobre segredos ocultos entre estantes antigas de uma biblioteca esquecida."
+    ],
+    [
+        "id" => 2,
+        "titulo" => "Viagem ao Centro do Tempo",
+        "imagem" => "https://exemplo.com/imagens/viagem-tempo.jpg",
+        "autor" => "Lucas Farias",
+        "avaliacao" => 4,
+        "descricao" => "Um cientista constrói uma máquina do tempo e embarca em uma jornada perigosa pelo passado e futuro."
+    ],
+    [
+        "id" => 3,
+        "titulo" => "Café com Poesia",
+        "imagem" => "https://exemplo.com/imagens/cafe-poesia.jpg",
+        "autor" => "Marina Duarte",
+        "avaliacao" => 2,
+        "descricao" => "Uma coletânea de poesias que falam de amor, saudade e manhãs regadas a café."
+    ],
+    [
+        "id" => 4,
+        "titulo" => "A Última Cidade",
+        "imagem" => "https://exemplo.com/imagens/ultima-cidade.jpg",
+        "autor" => "André Souza",
+        "avaliacao" => 2,
+        "descricao" => "Num mundo pós-apocalíptico, um grupo luta para sobreviver na última cidade habitável da Terra."
+    ],
+    [
+        "id" => 5,
+        "titulo" => "Entre Dois Mundos",
+        "imagem" => "https://exemplo.com/imagens/entre-mundos.jpg",
+        "autor" => "Beatriz Lima",
+        "avaliacao" => 1,
+        "descricao" => "Uma adolescente descobre que pertence a um universo paralelo e precisa escolher entre dois destinos."
+    ],
+    [
+        "id" => 6,
+        "titulo" => "O Código da Montanha",
+        "imagem" => "https://exemplo.com/imagens/codigo-montanha.jpg",
+        "autor" => "Renato Campos",
+        "avaliacao" => 4,
+        "descricao" => "Um alpinista encontra um antigo manuscrito codificado escondido no Himalaia."
+    ],
+    [
+        "id" => 7,
+        "titulo" => "A Ilha dos Esquecidos",
+        "imagem" => "https://exemplo.com/imagens/ilha-esquecidos.jpg",
+        "autor" => "Juliana Pires",
+        "avaliacao" => 3,
+        "descricao" => "Exploradores descobrem uma ilha que não aparece em nenhum mapa e escondem segredos milenares."
+    ],
+    [
+        "id" => 8,
+        "titulo" => "Notas de um Coração Partido",
+        "imagem" => "https://exemplo.com/imagens/coracao-partido.jpg",
+        "autor" => "Rafael Costa",
+        "avaliacao" => 3,
+        "descricao" => "Um romance que revela as cicatrizes do amor e a beleza da reconstrução emocional."
+    ],
+    [
+        "id" => 9,
+        "titulo" => "Códigos e Sombras",
+        "imagem" => "https://exemplo.com/imagens/codigos-sombras.jpg",
+        "autor" => "Isabela Nunes",
+        "avaliacao" => 4,
+        "descricao" => "Um hacker enfrenta uma organização secreta enquanto tenta limpar seu nome."
+    ]
+];
+
+?>
